98dbf1132d1e6da20d2210a955dc4ecb8399b0a2
Saurabh Patil <100773962+saurabhp536@users.noreply.github.com>
2026-05-18 12:42:41 +0530

    Change base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-  base: "/e-commerce-customer-retention-dashboard/",
+  base: "/Ecommerce-dashboard/",
 
   plugins: [react(), tailwindcss(), viteSingleFile()],
 
